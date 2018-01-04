59ef24e836fdd56ac9583a50e4799ff769d05899
153102082 <153102082@qq.com>
2018-01-04 19:40:40 +0800

    xiugai

--- js/ui.js
+++ js/ui.js
@@ -1,10 +1,29 @@
+var imgNum = 0;
+var images = [];
+function preLoadImg(){
+    $.imgpreload([],function (){});
+    images.push('img/001.png','img/002.png','img/003.png','img/004.png','img/005.png','img/006.png','img/007.png','img/008.png','img/009.png','img/a1-1.png','img/a1-2.png','img/a1-3.png','img/a1-4.png','img/a2-1.png','img/a2-2.png','img/a2-3.png','img/a2-4.png','img/a3-1.png','img/a3-2.png','img/a3-3.png','img/a3-4.png','img/a3-5.png','img/a3-6.png','img/a4-1.png','img/a4-2.png','img/a4-3.png','img/a4-4.png','img/a5-1.png','img/a5-2.png','img/a5-3.png','img/a5-4.png','img/a5-5.png','img/a5-6.png','img/a6-1.png','img/a6-2.png','img/a6-4.png','img/a7-1.png','img/a7-2.png','img/a7-4.png','img/an1-1.png','img/an1-2.png','img/an2-1.png','img/an2-2.png','img/an3-1.png','img/an3-2.png','img/B1.png','img/B2.png','img/B3.png','img/B4.png','img/bg-a1.jpg','img/bg-a2.jpg','img/bg-a3.jpg','img/bg-a4.jpg','img/bg-a5.jpg','img/bg-a6.jpg','img/bg-a7.jpg','img/bg1.jpg','img/bg2.jpg','img/bg2.png','img/bg3-1.jpg','img/bg3-1.png','img/bg3-2.jpg','img/bg3-3.jpg','img/bg3.jpg','img/btn.png','img/but1.png','img/but2.png','img/car2.png','img/cj_btn.png','img/cx.png','img/fx.jpg','img/fx_btn.png','img/gz.png','img/gz1.png','img/info.png','img/J1.png','img/J2.png','img/J3.png','img/J4.png','img/J5.png','img/logo.png','img/music.mp3','img/music.png','img/p1-1.png','img/p1-2.png','img/p2-1.png','img/p3-1.png','img/p4-1.png','img/p5-1.png','img/p6-1.png','img/p7-1.png','img/p7-2.png','img/p8-1.png','img/p9-1.png','img/p9-2.png','img/p9-2a.png','img/p9-2b.png','img/p9-2c.png','img/p9-2d.png','img/p9-2e.png','img/p9-2f.png','img/p9-2g.png','img/p9-2h.png','img/p9-3.png','img/p9-4.png','img/p9-5.png','img/p9-5a.png','img/p9-5b.png','img/p9-5c.png','img/p9-5d.png','img/p9-5e.png','img/p9-5f.png','img/p9-5g.png','img/p9-5h.png','img/qd_btn.png','img/tj.png','img/ts.png','img/ts1.png','img/turnplate-bg.png','img/turnplate-pointer.png','img/y1.png','img/y2.png','img/y3.png','img/yao.mp3');
+    $.imgpreload(images,{
+        each: function () {
+            var status = $(this).data('loaded') ? 'success' : 'error';
+            if (status == "success") {}
+        }
+
+    });
+}
+
+
 setTimeout(function(){
     $(".loading").hide();
     $(".wrap").show();
 
-},1000);
+},4000);
 
 $(function(){
+    preLoadImg();
+
+
+
     var time1=setInterval(function(){
         $(".animate1").css("left",($(".animate1").offset().left-15));
 
